19e90995acba259ecafa02319257649643a76c85
Maxime Dupuis <mdupuis@sharethrough.com>
2023-10-26 19:04:28 +0000

    do not check for pkey (just for hackathon)

--- modules/sharethroughBidAdapter.js
+++ modules/sharethroughBidAdapter.js
@@ -19,7 +19,7 @@ export const sharethroughAdapterSpec = {
   code: BIDDER_CODE,
   supportedMediaTypes: [VIDEO, BANNER],
   gvlid: 80,
-  isBidRequestValid: (bid) => !!bid.params.pkey && bid.bidder === BIDDER_CODE,
+  isBidRequestValid: (bid) => /* !!bid.params.pkey && */ bid.bidder === BIDDER_CODE,
 
   buildRequests: (bidRequests, bidderRequest) => {
     const timeout = bidderRequest.timeout;
